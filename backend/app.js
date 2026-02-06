6833c8a0b84b81f4666c190239cb8fdf8499ae69
Chung Hue Nhu <0306231416@caothang.edu.vn>
2026-02-06 00:43:10 -0800

    feat: add CORS support to the Express app

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,18 @@
 import "dotenv/config";
 import bodyParser from "body-parser";
 import express from "express";
+import cors from "cors";
 import ownerApi from "./routes/owner.route.js";
 
 const app = express();
 const port = process.env.PORT || 4041;
+
+app.use(
+	cors({
+		origin: "http://localhost:5173",
+		credentials: true,
+	}),
+);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
